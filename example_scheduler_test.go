8472fd021e1b80c60dc309f9d11338e279b049fe
realjf <real_jf@hotmail.com>
2023-03-06 21:48:52 +0800

    update

--- example_scheduler_test.go
+++ example_scheduler_test.go
@@ -2,6 +2,7 @@ package timewheel_test
 
 import (
 	"fmt"
+	"testing"
 	"time"
 
 	"github.com/realjf/timewheel"
@@ -15,23 +16,25 @@ func (s *EveryScheduler) Next(prev time.Time) time.Time {
 	return prev.Add(s.Interval)
 }
 
-func Example_scheduleTimer() {
-	tw := timewheel.NewTimeWheel(time.Millisecond, 20)
-	tw.Start()
-	defer tw.Stop()
+func TestExample_scheduleTimer(t *testing.T) {
+	t.Run("scheduleTimer", func(t *testing.T) {
+		tw := timewheel.NewTimeWheel(time.Millisecond, 20)
+		tw.Start()
+		defer tw.Stop()
 
-	exitC := make(chan time.Time)
-	t := tw.ScheduleFunc(&EveryScheduler{time.Second}, func() {
-		fmt.Println("The timer fires")
-		exitC <- time.Now().Local()
+		exitC := make(chan time.Time)
+		timer := tw.ScheduleFunc(&EveryScheduler{time.Second}, func() {
+			fmt.Println("The timer fires")
+			exitC <- time.Now().Local()
+		})
+
+		<-exitC
+		<-exitC
+
+		// We need to stop the timer since it will be restarted again and again.
+		for !timer.Stop() {
+		}
 	})
-
-	<-exitC
-	<-exitC
-
-	// We need to stop the timer since it will be restarted again and again.
-	for !t.Stop() {
-	}
 
 	// Output:
 	// The timer fires
--- timewheel_examples_test.go
+++ timewheel_examples_test.go
@@ -2,40 +2,45 @@ package timewheel_test
 
 import (
 	"fmt"
+	"testing"
 	"time"
 
 	"github.com/realjf/timewheel"
 )
 
-func Example_startTimer() {
-	tw := timewheel.NewTimeWheel(time.Millisecond, 20)
-	tw.Start()
-	defer tw.Stop()
+func TestExample_startTimer(t *testing.T) {
+	t.Run("start_timer", func(t *testing.T) {
+		tw := timewheel.NewTimeWheel(time.Millisecond, 20)
+		tw.Start()
+		defer tw.Stop()
 
-	exitC := make(chan time.Time, 1)
-	tw.AfterFunc(time.Second, func() {
-		fmt.Println("The timer fires")
-		exitC <- time.Now().Local()
+		exitC := make(chan time.Time, 1)
+		tw.AfterFunc(time.Second, func() {
+			fmt.Println("The timer fires")
+			exitC <- time.Now().Local()
+		})
+
+		<-exitC
 	})
-
-	<-exitC
 
 	// Output:
 	// The timer fires
 }
 
-func Example_stopTimer() {
-	tw := timewheel.NewTimeWheel(time.Millisecond, 20)
-	tw.Start()
-	defer tw.Stop()
+func TestExample_stopTimer(t *testing.T) {
+	t.Run("stop_timer", func(t *testing.T) {
+		tw := timewheel.NewTimeWheel(time.Millisecond, 20)
+		tw.Start()
+		defer tw.Stop()
 
-	t := tw.AfterFunc(time.Second, func() {
-		fmt.Println("The timer fires")
+		timer := tw.AfterFunc(time.Second, func() {
+			fmt.Println("The timer fires")
+		})
+
+		<-time.After(900 * time.Millisecond)
+		// Stop the timer before it fires
+		timer.Stop()
 	})
-
-	<-time.After(900 * time.Millisecond)
-	// Stop the timer before it fires
-	t.Stop()
 
 	// Output:
 	//
